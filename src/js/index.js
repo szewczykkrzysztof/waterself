169e7a577162e6fbe6782a837d1443c04e0e4438
Krzysztof Szewczyk <szewczykris@gmail.com>
2020-10-28 23:16:48 +0100

    added element query selectors

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,7 @@ registerSW();
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+const glassCounter = document.querySelector(".glass__count-js");
+const addGlass = document.querySelector(".addGlass-js");
+const removeGlass = document.querySelector(".removeGlass-js");
