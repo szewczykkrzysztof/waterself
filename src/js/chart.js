a9c4778469592923ed6e3fd4975d1cae47eddbad
Krzysztof Szewczyk <szewczykris@gmail.com>
2020-11-14 08:58:29 +0100

    created loop wchich listing date of past 10 days

--- src/js/chart.js
+++ src/js/chart.js
@@ -1,3 +1,18 @@
 import "../scss/main.scss";
 
-console.log('Hello')
+// tworze zmienna z aktulną datą i czasem
+var today = new Date();
+
+// pętla wykona się i+1 razy
+for (let i = 10; i > -1; i--) {
+
+//    odejmuję od aktualnego czasu i * doba wyrażona w milisekundach
+   var numberTimeValue = (today -(i*86400000));
+
+   //    przypisuję do zmiennej nową datę utworzoną z milisekund
+   var historicTime = new Date(numberTimeValue);
+
+   //    przekształcam datę na format iso i wycinam tylko datę
+   var isoDay = historicTime.toISOString().slice(0, 10);
+   console.log(isoDay);
+}
