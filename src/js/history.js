e5169e376cfab3c39da18bbce83373d0cf78e8cf
Krzysztof Szewczyk <szewczykris@gmail.com>
2020-11-16 21:23:43 +0100

    temporary history version created for import data from previous day individual format to single JSON table

--- src/js/history.js
+++ src/js/history.js
@@ -6,6 +6,9 @@ const results = document.querySelector(".waterHistory--js");
 // wycinam z aktualnej daty ISO 4 znaki (rok) i przypisuję do zmiennej
 let year = new Date().toISOString().slice(0, 4);
 
+// tworzę tablicę do przechowywania rekordów dziennych
+var drinkTable = [];
+
 for (let key in localStorage) {
   // wycianam pierwsze rok z pobranego klucza
   const keyYear = key.slice(0, 4);
@@ -14,7 +17,10 @@ for (let key in localStorage) {
   var newParagraph = document.createElement("p");
 
   if (keyYear === year) {
-    console.log(`${key} wypiłeś: ${localStorage.getItem(key)}`);
+    // przypisuję wartość klucza do zmiennej
+    var keyValue = localStorage.getItem(key);
+
+    console.log(`${key} wypiłeś: ${keyValue}`);
 
     // dodaje nowy paragraf do sekcji results
     results.appendChild(newParagraph);
@@ -23,11 +29,27 @@ for (let key in localStorage) {
     newParagraph.setAttribute("class", "waterHistory--record");
 
     // tworzę zawartość wpisu pobraną z zapisanego obiektu
-    var newParagraphContent = document.createTextNode(
-      `${key} : ${localStorage.getItem(key)}`
-    );
+    var newParagraphContent = document.createTextNode(`${key} : ${keyValue}`);
 
     // dodaję do utworzonego paragrafu utworzoną zawartość
     newParagraph.appendChild(newParagraphContent);
+
+    // funkcja tworząca nowy rekord
+    function record(inputData, inputGlassValue) {
+      var arrayRecord = {
+        data: inputData,
+        // ilość szklanek jest pobierana z wartości klucza
+        glassCount: inputGlassValue,
+      };
+      return arrayRecord;
+    }
+
+    // wywołuje funkcję tworzącą obiekt z wpisem z jednego dnia
+    var oneDayRecord = record(key, keyValue);
+    drinkTable.push(oneDayRecord);
+    console.log(drinkTable);
   }
 }
+
+// zapisuję tablice do localstorage z wykorzystaniem kodowania obiektów JSON
+localStorage.setItem("glassHistory", JSON.stringify(drinkTable));
