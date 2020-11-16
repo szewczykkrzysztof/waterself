f9ab366896d62c11f09cbc3cd8d2ae2fe4f26dca
Krzysztof Szewczyk <szewczykris@gmail.com>
2020-11-17 00:51:43 +0100

    new history version operated on data stored in localstorage in one array

--- src/js/history.js
+++ src/js/history.js
@@ -1,55 +1,48 @@
 import "../scss/main.scss";
 
-// podpinam klasę zawierającą historię nawodnienia do zmiennej
+// podpinam klasę pod którą pojawi się historię nawodnienia do zmiennej
 const results = document.querySelector(".waterHistory--js");
 
-// wycinam z aktualnej daty ISO 4 znaki (rok) i przypisuję do zmiennej
-let year = new Date().toISOString().slice(0, 4);
+// ładuję tablicę z historią z local storage poprzez odkodowanie JSON
+var drinkTable = JSON.parse(localStorage.getItem("glassHistory"));
 
-// tworzę tablicę do przechowywania rekordów dziennych
-var drinkTable = [];
+// tworzę pętlę, która będzie wykonywana dla 30 elementów z tablicy
+for (let i = 0; i <= 29; i++) {
+  // generuję dzisiejszą datę
+  let today = new Date();
 
-for (let key in localStorage) {
-  // wycianam pierwsze rok z pobranego klucza
-  const keyYear = key.slice(0, 4);
+  // ustawiam datę z historii
+  today.setDate(today.getDate() - i);
 
-  //   dodaje zmienną tworzącą tworzącą nowy paragraf
-  var newParagraph = document.createElement("p");
+  // konweruję datę na format ISO i wycinam samą datę
+  let todayISO = today.toISOString().slice(0, 10);
 
-  if (keyYear === year) {
-    // przypisuję wartość klucza do zmiennej
-    var keyValue = localStorage.getItem(key);
+  // szukam daty w tablicy i zwracam index elementu przy użyciu funkcji szczałkowej
+  var record = drinkTable.find(({ data }) => data === todayISO);
+  console.log(record);
 
-    console.log(`${key} wypiłeś: ${keyValue}`);
+  // sprawdzam czy rekord istnieje
+  if (record) {
+    // wyszukuję index znalezionego rekordu
+    var position = drinkTable.indexOf(record);
+    console.log(position);
+    // wysiagam datę rekordu(wpisu)
+    var entryDate = drinkTable[position].data;
+    // wyciągam z obiektu ilość zapisanych szklanek
+    var entryValue = drinkTable[position].glassCount;
+    //   dodaje zmienną tworzącą tworzącą nowy paragraf
+    var newParagraph = document.createElement("p");
 
-    // dodaje nowy paragraf do sekcji results
+    // dodaje nowy paragraf do sekcji results (tworzę jego dziecko)
     results.appendChild(newParagraph);
 
     // dodaje atrybut do paragrafu
     newParagraph.setAttribute("class", "waterHistory--record");
 
-    // tworzę zawartość wpisu pobraną z zapisanego obiektu
-    var newParagraphContent = document.createTextNode(`${key} : ${keyValue}`);
-
-    // dodaję do utworzonego paragrafu utworzoną zawartość
+    // tworzę zawartość węzła tekstowego z wyciągniętych z obiektu wartości
+    var newParagraphContent = document.createTextNode(`${entryDate} : ${entryValue}`);
+    
+    // dodaję do utworzonego paragrafu storzony tekst
     newParagraph.appendChild(newParagraphContent);
-
-    // funkcja tworząca nowy rekord
-    function record(inputData, inputGlassValue) {
-      var arrayRecord = {
-        data: inputData,
-        // ilość szklanek jest pobierana z wartości klucza
-        glassCount: inputGlassValue,
-      };
-      return arrayRecord;
-    }
-
-    // wywołuje funkcję tworzącą obiekt z wpisem z jednego dnia
-    var oneDayRecord = record(key, keyValue);
-    drinkTable.push(oneDayRecord);
-    console.log(drinkTable);
   }
 }
-
-// zapisuję tablice do localstorage z wykorzystaniem kodowania obiektów JSON
-localStorage.setItem("glassHistory", JSON.stringify(drinkTable));
