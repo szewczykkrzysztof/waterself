384ee20531bf7b4f2bbbfe8895fb276c1afc299a
Krzysztof Szewczyk <szewczykris@gmail.com>
2020-11-19 22:59:23 +0100

    moved generating chart to function, link click action to navigation element, add action to generate week and month chart

--- src/js/chart.js
+++ src/js/chart.js
@@ -10,49 +10,75 @@ console.log(`Zapisanych zostało ${historyLength} wpisów w historii`);
 //   tworze połacznie z klasą wykresu
 var chartBody = document.querySelector(".chart__body");
 
-// tworzę pętlę, która będzie wykonywana dla 30 elementów z tablicy
-for (let i = 0; i <= 29; i++) {
-  // generuję dzisiejszą datę
-  let today = new Date();
+// tworzę połaczenia z przyciskami do zmiany typu wykresu
+const chartWeekButton = document.querySelector(".chart__week--js");
+const chartMonthButton = document.querySelector(".chart__month--js");
 
-  // ustawiam datę z historii
-  today.setDate(today.getDate() - i);
+generateChart("week");
 
-  // konweruję datę na format ISO i wycinam samą datę
-  let todayISO = today.toISOString().slice(0, 10);
+// podpinam nasłuch na klik do przycisków
+chartWeekButton.addEventListener("click", (e) => generateChart("week"));
+chartMonthButton.addEventListener("click", (e) => generateChart("month"));
 
-  // szukam daty w tablicy i przypisuje element do zmiennej przy użyciu funkcji szczałkowej
-  var record = drinkTable.find(({ data }) => data === todayISO);
-  console.log(record);
-
-  // funkcja tworząca nowy blok
-  function addGraphBar(entryData,entryValue) {
-    //   przypinam do zmiennej stworzenie nowego słupka wykresu
-    var newBox = document.createElement("div");
-    // dodaje nowy słupek do wykresu (tworzę dziecko wykresu)
-    chartBody.appendChild(newBox);
-    // dodaje atrybut klasy do stworzonego słupka
-    newBox.setAttribute("class", "chart__oneDay");
-    //  ustalam wyskość słupka wykresu
-    var barHeight = entryValue * 10;
-    //   dodaje wysokosc słupka do zmiennej css
-    newBox.style.setProperty("--barHeight", `${barHeight}%`);
+function generateChart(chartType) {
+  switch (chartType) {
+    case "week":
+      console.log(`Wybrałeś ${chartType}`);
+      newChart(7);
+      // // zamienia pomiedzy przyciskami klasę na active
+      break;
+    case "month":
+      console.log(`Wybrałeś ${chartType}`);
+      newChart(30);
+      // zamienia pomiedzy przyciskami klasę na active
+      break;
   }
+}
+function newChart(daysNumber) {
+  // tworzę pętlę wykonywaną dla zdefiniowanej ilości dnii
+  for (let i = 0; i <= daysNumber; i++) {
+    // generuję dzisiejszą datę
+    let today = new Date();
 
-  // sprawdzam czy rekord istnieje
-  if (record) {
-    // wyszukuję index znalezionego rekordu
-    var position = drinkTable.indexOf(record);
-    console.log(position);
-    // wysiagam datę rekordu(wpisu)
-    var recordDate = drinkTable[position].data;
-    // wyciągam z obiektu ilość zapisanych szklanek
-    var recordValue = drinkTable[position].glassCount;
+    // ustawiam datę z historii
+    today.setDate(today.getDate() - i);
 
-    //   wywołuję funkcję dodającą nowy słupek wykresu
-    addGraphBar(recordDate,recordValue);
-  } else {
-    // wywołanie funkcji dodającej słupek dla braku wpisu
-    addGraphBar(recordDate,0);
+    // konweruję datę na format ISO i wycinam samą datę
+    let todayISO = today.toISOString().slice(0, 10);
+
+    // szukam daty w tablicy i przypisuje element do zmiennej przy użyciu funkcji szczałkowej
+    var record = drinkTable.find(({ data }) => data === todayISO);
+    console.log(record);
+
+    // funkcja tworząca nowy blok
+    function addGraphBar(entryData, entryValue) {
+      //   przypinam do zmiennej stworzenie nowego słupka wykresu
+      var newBox = document.createElement("div");
+      // dodaje nowy słupek do wykresu (tworzę dziecko wykresu)
+      chartBody.appendChild(newBox);
+      // dodaje atrybut klasy do stworzonego słupka
+      newBox.setAttribute("class", "chart__oneDay");
+      //  ustalam wyskość słupka wykresu
+      var barHeight = entryValue * 10;
+      //   dodaje wysokosc słupka do zmiennej css
+      newBox.style.setProperty("--barHeight", `${barHeight}%`);
+    }
+
+    // sprawdzam czy rekord istnieje
+    if (record) {
+      // wyszukuję index znalezionego rekordu
+      var position = drinkTable.indexOf(record);
+      console.log(position);
+      // wysiagam datę rekordu(wpisu)
+      var recordDate = drinkTable[position].data;
+      // wyciągam z obiektu ilość zapisanych szklanek
+      var recordValue = drinkTable[position].glassCount;
+
+      //   wywołuję funkcję dodającą nowy słupek wykresu
+      addGraphBar(recordDate, recordValue);
+    } else {
+      // wywołanie funkcji dodającej słupek dla braku wpisu
+      addGraphBar(recordDate, 0);
+    }
   }
 }
