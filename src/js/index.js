66c9bcecbc60fa7988fb1a73201ae0ebf50eda0b
Krzysztof Szewczyk <szewczykris@gmail.com>
2020-11-24 11:48:57 +0100

    moved function for manipulating (adding, substracting) glass value to the end of code for improve readability

--- src/js/index.js
+++ src/js/index.js
@@ -23,7 +23,7 @@ if (savingHistory) {
   // attach last element from history array
   var lastEntry = savingHistory[savingHistory.length - 1];
   console.log(
-    `Ostatni element z historii to ${lastEntry.data} a liczba szklanek to ${lastEntry.glassCount}`
+    `Ostatni element z historii to ${lastEntry} a liczba szklanek to ${lastEntry.glassCount}`
   );
   // check if last entry is saved today
   if (lastEntry.data === today) {
@@ -57,39 +57,6 @@ if (savingHistory) {
   glassCounter.innerHTML = 0;
 }
 
-// function which do operation on last record in history table
-
-function entryManipulate(glassValueToAdd) {
-  // check if last entry is saved today
-  if (lastEntry.data === today) {
-    // last record is saved in variable, remove last array element to do space on updated record
-    savingHistory.pop();
-    // check if stored glass value > 0
-    if ((storageCounter) => 0) {
-      // add or substract glass to counter
-      storageCounter = storageCounter + glassValueToAdd;
-      // if storage dropped below 0, set counter on 0
-      if (storageCounter < 0) {
-        storageCounter = 0;
-      }
-    }
-  }
-
-  console.log(`Koniec petli, dodaję ${storageCounter} do htmla i zapisuję do localstorage`);
-
-  // inject glass number value to html element
-  glassCounter.innerHTML = storageCounter;
-
-  // add today record to array
-  savingHistory.push({ data: today, glassCount: storageCounter });
-
-  // remove old localstorage key
-  localStorage.removeItem("glassHistory");
-
-  // // add updated key to local storage`
-  localStorage.setItem("glassHistory", JSON.stringify(savingHistory));
-}
-
 // Add +1 to glass counter on localstorage
 addGlass.addEventListener("click", (e) => {
   // prevent from preloading page
@@ -110,3 +77,36 @@ removeGlass.addEventListener("click", (e) => {
     e.preventDefault();
   }
 });
+
+// function which do operation on last record in history table
+
+function entryManipulate(glassValueToAdd) {
+  // check if last entry is saved today
+  if (lastEntry.data === today) {
+    // last record is saved in variable, remove last array element to do space on updated record
+    savingHistory.pop();
+    // check if stored glass value > 0
+    if ((storageCounter) => 0) {
+      // add or substract glass to counter
+      storageCounter = storageCounter + glassValueToAdd;
+      // if storage dropped below 0, set counter on 0
+      if (storageCounter < 0) {
+        storageCounter = 0;
+      }
+    }
+  }
+
+  // inject glass number value to html element
+  glassCounter.innerHTML = storageCounter;
+
+  // add today record to array
+  savingHistory.push({ data: today, glassCount: storageCounter });
+
+  // remove old localstorage key
+  localStorage.removeItem("glassHistory");
+
+  // // add updated key to local storage`
+  localStorage.setItem("glassHistory", JSON.stringify(savingHistory));
+
+  console.log(`Koniec petli, lokalny licznik: ${storageCounter}, lcznik z localstorage ${savingHistory[savingHistory.length-1].glassCount}`);
+}
