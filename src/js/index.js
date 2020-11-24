12765a289918e0b690de596054e67a6963d0c8ce
Krzysztof Szewczyk <szewczykris@gmail.com>
2020-11-24 11:15:47 +0100

    repair  behaviour when no record is saved for today => set local glass counter on 0 when no today record, moved inject glass number to html to root of loop checking today record existing. Add some monitoring console.logs and improve comments

--- src/js/index.js
+++ src/js/index.js
@@ -22,25 +22,37 @@ let savingHistory = JSON.parse(localStorage.getItem(`glassHistory`));
 if (savingHistory) {
   // attach last element from history array
   var lastEntry = savingHistory[savingHistory.length - 1];
+  console.log(
+    `Ostatni element z historii to ${lastEntry.data} a liczba szklanek to ${lastEntry.glassCount}`
+  );
   // check if last entry is saved today
   if (lastEntry.data === today) {
-    var storageCounter = lastEntry.glassCount;
     // assign saved record value to counter variable
+    var storageCounter = lastEntry.glassCount;
+    // check if saved value is a number
     if (typeof storageCounter == "number") {
       console.log(`Z historii wczytano: ${storageCounter} szklanek`);
     } else {
       storageCounter = 0;
+      console.log(
+        "Niestety zapisana ilość szklanek nie jest liczbą, ustawiam 0"
+      );
     }
-    // inject stored counter value to html counter
-    glassCounter.innerHTML = storageCounter;
+  } else {
+    console.log("Dzisiaj jeszcze nic nie dodałeś");
+    // if no record for today set local glass counter to 0
+    storageCounter = 0;
   }
+  // inject stored counter value to html counter
+  glassCounter.innerHTML = storageCounter;
 } else {
   // create today initial record
   var lastEntry = { data: today, glassCount: 0 };
-  // create array with today record
+  // create new array with today record
   savingHistory = [lastEntry];
-  // save table to local storage
+  // save array to local storage
   localStorage.setItem("glassHistory", JSON.stringify(savingHistory));
+  // set local glass counter variable
   var storageCounter = 0;
   glassCounter.innerHTML = 0;
 }
@@ -48,10 +60,8 @@ if (savingHistory) {
 // function which do operation on last record in history table
 
 function entryManipulate(glassValueToAdd) {
-
   // check if last entry is saved today
   if (lastEntry.data === today) {
-
     // last record is saved in variable, remove last array element to do space on updated record
     savingHistory.pop();
     // check if stored glass value > 0
@@ -64,6 +74,9 @@ function entryManipulate(glassValueToAdd) {
       }
     }
   }
+
+  console.log(`Koniec petli, dodaję ${storageCounter} do htmla i zapisuję do localstorage`);
+
   // inject glass number value to html element
   glassCounter.innerHTML = storageCounter;
 
@@ -93,6 +106,7 @@ removeGlass.addEventListener("click", (e) => {
     // execute function with substract value
     entryManipulate(-1);
   } else {
+    // if glass counter = 0 no reaction (prevent reloading page)
     e.preventDefault();
   }
 });
