1c4d6ddb73cf1ab203cfa78137b9c4572fd2cd2c
Krzysztof Szewczyk <szewczykris@gmail.com>
2020-11-25 15:35:52 +0100

    add function which generate drinking history to chart.html, add drinking 7 days list below chart

--- src/js/chart.js
+++ src/js/chart.js
@@ -22,6 +22,7 @@ const chartMonthButton = document.querySelector(".chart__month--js");
 
 // wywolanie generowania wykresu tygodniowego pod załadowaniu strony
 generateChart("week");
+generateDrinkHistory(7);
 
 // podpinam nasłuch na klik do przycisków
 chartWeekButton.addEventListener("click", (e) => generateChart("week"));
@@ -167,6 +168,64 @@ function newChart(daysNumber) {
       if (i % 5 == 0) {
         shortDateDescription(dateFromHistory);
       }
+    }
+  }
+}
+
+function generateDrinkHistory(inputLength) {
+  for (let i = 0; i <= inputLength - 1; i++) {
+    // generuję dzisiejszą datę
+    let today = new Date();
+
+    // ustawiam datę z historii
+    today.setDate(today.getDate() - i);
+
+    // konweruję datę na format ISO i wycinam samą datę
+    let todayISO = today.toISOString().slice(0, 10);
+
+    // szukam daty w tablicy i przypisuje element do zmiennej przy użyciu funkcji szczałkowej
+    var record = drinkTable.find(({ data }) => data === todayISO);
+    console.log(record);
+
+    // tworze funkcję dodającą nową linię w dokumencie html
+
+    function addParagraph(entryDate, entryValue) {
+      //   dodaje zmienną tworzącą tworzącą nowy paragraf
+      var newParagraph = document.createElement("p");
+
+      // dodaje nowy paragraf do sekcji results (tworzę jego dziecko)
+      results.appendChild(newParagraph);
+
+      // dodaje atrybut do paragrafu
+      newParagraph.setAttribute("class", "waterHistory--record");
+
+      // tworzę zawartość węzła tekstowego z wyciągniętych z obiektu wartości
+      var newParagraphContent = document.createTextNode(
+        `${entryDate} : ${entryValue}`
+      );
+
+      // dodaję do utworzonego paragrafu storzony tekst
+      newParagraph.appendChild(newParagraphContent);
+    }
+
+    // sprawdzam czy rekord istnieje
+    if (record) {
+      // wyszukuję index znalezionego rekordu
+      var position = drinkTable.indexOf(record);
+      console.log(position);
+      // wysiagam datę rekordu(wpisu)
+      var recordDate = drinkTable[position].data;
+      // wyciągam z obiektu ilość zapisanych szklanek
+      var recordValue = drinkTable[position].glassCount;
+      // przeliczam szklanki na litry
+      recordValue = recordValue * 0.25;
+      // dodaję symbol litra
+      recordValue = recordValue + "  l";
+      // wywołuję fukcję dodającą nowa linie z danymi wyciagniętymi z pobranego rekordu
+      addParagraph(recordDate, recordValue);
+    } else {
+      // wywołuję dodanie nowej linii z aktualnie tetstowaną datą i wartością 0
+      addParagraph(todayISO, "0");
     }
   }
 }
