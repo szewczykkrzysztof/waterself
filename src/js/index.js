520c4fe2291405deff8993b6ed75a091f86cbc0d
Krzysztof Szewczyk <szewczykris@gmail.com>
2020-11-25 10:00:19 +0100

    move local counter variable declaration to begining of code

--- src/js/index.js
+++ src/js/index.js
@@ -15,11 +15,14 @@ const removeGlass = document.querySelector(".removeGlass-js");
 const today = new Date().toISOString().slice(0, 10);
 
 // Load drink history from localstorage
-
 let savingHistory = JSON.parse(localStorage.getItem(`glassHistory`));
+
+// add local counter of drinking glass
+let storageCounter = 0;
 
 // check if history was stored in localstorage
 if (savingHistory) {
+  console.log("Historia wpisów jest już zapisana w local storage");
   // attach last element from history array
   var lastEntry = savingHistory[savingHistory.length - 1];
   console.log(
@@ -27,8 +30,9 @@ if (savingHistory) {
   );
   // check if last entry is saved today
   if (lastEntry.data === today) {
+    console.log(`W tablicy istnieje wpis z dzisiejszego dnia ${lastEntry}`)
     // assign saved record value to counter variable
-    var storageCounter = lastEntry.glassCount;
+    storageCounter = lastEntry.glassCount;
     // check if saved value is a number
     if (typeof storageCounter == "number") {
       console.log(`Z historii wczytano: ${storageCounter} szklanek`);
@@ -45,6 +49,7 @@ if (savingHistory) {
   }
   // inject stored counter value to html counter
   glassCounter.innerHTML = storageCounter;
+  console.log("Wyświetliłem zapisany licznik na stronie");
 } else {
   // create today initial record
   var lastEntry = { data: today, glassCount: 0 };
@@ -53,7 +58,7 @@ if (savingHistory) {
   // save array to local storage
   localStorage.setItem("glassHistory", JSON.stringify(savingHistory));
   // set local glass counter variable
-  var storageCounter = 0;
+  storageCounter = 0;
   glassCounter.innerHTML = 0;
 }
 
@@ -75,6 +80,7 @@ removeGlass.addEventListener("click", (e) => {
   } else {
     // if glass counter = 0 no reaction (prevent reloading page)
     e.preventDefault();
+    console.log("Wartość nie może być ujemna");
   }
 });
 
@@ -85,10 +91,12 @@ function entryManipulate(glassValueToAdd) {
   if (lastEntry.data === today) {
     // last record is saved in variable, remove last array element to do space on updated record
     savingHistory.pop();
+    console.log(`Dzisiejszy wpis istnieje: ${lastEntry}`);
     // check if stored glass value > 0
     if ((storageCounter) => 0) {
       // add or substract glass to counter
       storageCounter = storageCounter + glassValueToAdd;
+      console.log(`Po dodaniu stan wewnętrznego licznika to: ${storageCounter}`);
       // if storage dropped below 0, set counter on 0
       if (storageCounter < 0) {
         storageCounter = 0;
@@ -101,12 +109,15 @@ function entryManipulate(glassValueToAdd) {
 
   // add today record to array
   savingHistory.push({ data: today, glassCount: storageCounter });
+  console.log(`Do wewnętrznej tabeli dodano: ${data}: ${today}, ${glassCount}: ${storageCounter}}`);
 
   // remove old localstorage key
   localStorage.removeItem("glassHistory");
+  console.log("Usunięto starą tabelę w localstorage")
 
   // // add updated key to local storage`
   localStorage.setItem("glassHistory", JSON.stringify(savingHistory));
+  console.log("Zapisano nową tabelę w localstorage")
 
   console.log(`Koniec petli, lokalny licznik: ${storageCounter}, lcznik z localstorage ${savingHistory[savingHistory.length-1].glassCount}`);
 }
