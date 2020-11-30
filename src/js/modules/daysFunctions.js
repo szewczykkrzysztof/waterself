3fdf85ef5cc8e2ebec611d073058811e8f425b9f
Krzysztof Szewczyk <szewczykris@gmail.com>
2020-11-30 13:32:03 +0100

    add module with js function

---
+++ src/js/modules/daysFunctions.js
@@ -0,0 +1,10 @@
+// funkcja zwracająca skrót polskiego dnia tygodnia dla zadanej daty
+export function polishWeekDay(entryDate) {
+    console.log("Konwertuję na polski dzień tygodnia");
+    // definiuję tablicę z polskimi dniami tygodnia
+    const polishDay = ["Nie", "Pon", "Wto", "Śro", "Czw", "Pią", "Sob"];
+    // pobieram numer dnia tygodnia i podstawiam go jako index do tablicy z polskimi dniami tygodnia
+    var weekDay = polishDay[entryDate.getDay()];
+    // zwracam jako wynik działąnia funkcji wartość polskiego dnia tygonia
+    return weekDay;
+  }
